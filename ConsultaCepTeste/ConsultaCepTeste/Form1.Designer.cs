754dcd1e628ddb4d905b51df541bafca31fa3cc3
Virtthuos <84107409+Virtthuos@users.noreply.github.com>
2022-10-20 19:50:26 -0300

    ConsultaCEP API - ViaCEP
    Programa para consultar o CEP através da API pública do VIACEP.

---
+++ ConsultaCepTeste/ConsultaCepTeste/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace ConsultaCepTeste
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cepLabel = new System.Windows.Forms.Label();
+            this.consultaCepTextBox = new System.Windows.Forms.TextBox();
+            this.ConsultaBtn = new System.Windows.Forms.Button();
+            this.cepLabelRetorno = new System.Windows.Forms.Label();
+            this.logradouroLabel = new System.Windows.Forms.Label();
+            this.complementoLabel = new System.Windows.Forms.Label();
+            this.bairroLabel = new System.Windows.Forms.Label();
+            this.localidadeLabel = new System.Windows.Forms.Label();
+            this.ufLabel = new System.Windows.Forms.Label();
+            this.ibgeLabel = new System.Windows.Forms.Label();
+            this.giaLabel = new System.Windows.Forms.Label();
+            this.dddLabel = new System.Windows.Forms.Label();
+            this.siafiLabel = new System.Windows.Forms.Label();
+            this.cepTextBox = new System.Windows.Forms.TextBox();
+            this.logradouroTextBox = new System.Windows.Forms.TextBox();
+            this.complementoTextBox = new System.Windows.Forms.TextBox();
+            this.bairroTextBox = new System.Windows.Forms.TextBox();
+            this.LocalidadeTextBox = new System.Windows.Forms.TextBox();
+            this.UfTextBox = new System.Windows.Forms.TextBox();
+            this.ibgeTextBox = new System.Windows.Forms.TextBox();
+            this.giaTextBox = new System.Windows.Forms.TextBox();
+            this.dddTextBox = new System.Windows.Forms.TextBox();
+            this.siafiTextBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cepLabel
+            // 
+            this.cepLabel.AutoSize = true;
+            this.cepLabel.Location = new System.Drawing.Point(12, 9);
+            this.cepLabel.Name = "cepLabel";
+            this.cepLabel.Size = new System.Drawing.Size(28, 13);
+            this.cepLabel.TabIndex = 0;
+            this.cepLabel.Text = "CEP";
+            // 
+            // consultaCepTextBox
+            // 
+            this.consultaCepTextBox.Location = new System.Drawing.Point(12, 25);
+            this.consultaCepTextBox.Name = "consultaCepTextBox";
+            this.consultaCepTextBox.Size = new System.Drawing.Size(100, 20);
+            this.consultaCepTextBox.TabIndex = 1;
+            // 
+            // ConsultaBtn
+            // 
+            this.ConsultaBtn.Location = new System.Drawing.Point(118, 23);
+            this.ConsultaBtn.Name = "ConsultaBtn";
+            this.ConsultaBtn.Size = new System.Drawing.Size(30, 23);
+            this.ConsultaBtn.TabIndex = 2;
+            this.ConsultaBtn.Text = "OK";
+            this.ConsultaBtn.UseVisualStyleBackColor = true;
+            this.ConsultaBtn.Click += new System.EventHandler(this.ConsultaBtn_Click);
+            // 
+            // cepLabelRetorno
+            // 
+            this.cepLabelRetorno.AutoSize = true;
+            this.cepLabelRetorno.Location = new System.Drawing.Point(12, 87);
+            this.cepLabelRetorno.Name = "cepLabelRetorno";
+            this.cepLabelRetorno.Size = new System.Drawing.Size(26, 13);
+            this.cepLabelRetorno.TabIndex = 3;
+            this.cepLabelRetorno.Text = "Cep";
+            // 
+            // logradouroLabel
+            // 
+            this.logradouroLabel.AutoSize = true;
+            this.logradouroLabel.Location = new System.Drawing.Point(12, 120);
+            this.logradouroLabel.Name = "logradouroLabel";
+            this.logradouroLabel.Size = new System.Drawing.Size(61, 13);
+            this.logradouroLabel.TabIndex = 4;
+            this.logradouroLabel.Text = "Logradouro";
+            // 
+            // complementoLabel
+            // 
+            this.complementoLabel.AutoSize = true;
+            this.complementoLabel.Location = new System.Drawing.Point(12, 159);
+            this.complementoLabel.Name = "complementoLabel";
+            this.complementoLabel.Size = new System.Drawing.Size(71, 13);
+            this.complementoLabel.TabIndex = 5;
+            this.complementoLabel.Text = "Complemento";
+            // 
+            // bairroLabel
+            // 
+            this.bairroLabel.AutoSize = true;
+            this.bairroLabel.Location = new System.Drawing.Point(12, 196);
+            this.bairroLabel.Name = "bairroLabel";
+            this.bairroLabel.Size = new System.Drawing.Size(34, 13);
+            this.bairroLabel.TabIndex = 6;
+            this.bairroLabel.Text = "Bairro";
+            // 
+            // localidadeLabel
+            // 
+            this.localidadeLabel.AutoSize = true;
+            this.localidadeLabel.Location = new System.Drawing.Point(12, 231);
+            this.localidadeLabel.Name = "localidadeLabel";
+            this.localidadeLabel.Size = new System.Drawing.Size(59, 13);
+            this.localidadeLabel.TabIndex = 7;
+            this.localidadeLabel.Text = "Localidade";
+            // 
+            // ufLabel
+            // 
+            this.ufLabel.AutoSize = true;
+            this.ufLabel.Location = new System.Drawing.Point(12, 268);
+            this.ufLabel.Name = "ufLabel";
+            this.ufLabel.Size = new System.Drawing.Size(21, 13);
+            this.ufLabel.TabIndex = 8;
+            this.ufLabel.Text = "UF";
+            // 
+            // ibgeLabel
+            // 
+            this.ibgeLabel.AutoSize = true;
+            this.ibgeLabel.Location = new System.Drawing.Point(12, 307);
+            this.ibgeLabel.Name = "ibgeLabel";
+            this.ibgeLabel.Size = new System.Drawing.Size(32, 13);
+            this.ibgeLabel.TabIndex = 9;
+            this.ibgeLabel.Text = "IBGE";
+            // 
+            // giaLabel
+            // 
+            this.giaLabel.AutoSize = true;
+            this.giaLabel.Location = new System.Drawing.Point(12, 348);
+            this.giaLabel.Name = "giaLabel";
+            this.giaLabel.Size = new System.Drawing.Size(25, 13);
+            this.giaLabel.TabIndex = 10;
+            this.giaLabel.Text = "GIA";
+            // 
+            // dddLabel
+            // 
+            this.dddLabel.AutoSize = true;
+            this.dddLabel.Location = new System.Drawing.Point(12, 380);
+            this.dddLabel.Name = "dddLabel";
+            this.dddLabel.Size = new System.Drawing.Size(31, 13);
+            this.dddLabel.TabIndex = 11;
+            this.dddLabel.Text = "DDD";
+            // 
+            // siafiLabel
+            // 
+            this.siafiLabel.AutoSize = true;
+            this.siafiLabel.Location = new System.Drawing.Point(12, 415);
+            this.siafiLabel.Name = "siafiLabel";
+            this.siafiLabel.Size = new System.Drawing.Size(27, 13);
+            this.siafiLabel.TabIndex = 12;
+            this.siafiLabel.Text = "Siafi";
+            // 
+            // cepTextBox
+            // 
+            this.cepTextBox.Location = new System.Drawing.Point(89, 84);
+            this.cepTextBox.Name = "cepTextBox";
+            this.cepTextBox.Size = new System.Drawing.Size(100, 20);
+            this.cepTextBox.TabIndex = 13;
+            // 
+            // logradouroTextBox
+            // 
+            this.logradouroTextBox.Location = new System.Drawing.Point(89, 121);
+            this.logradouroTextBox.Name = "logradouroTextBox";
+            this.logradouroTextBox.Size = new System.Drawing.Size(216, 20);
+            this.logradouroTextBox.TabIndex = 14;
+            // 
+            // complementoTextBox
+            // 
+            this.complementoTextBox.Location = new System.Drawing.Point(89, 163);
+            this.complementoTextBox.Name = "complementoTextBox";
+            this.complementoTextBox.Size = new System.Drawing.Size(216, 20);
+            this.complementoTextBox.TabIndex = 15;
+            // 
+            // bairroTextBox
+            // 
+            this.bairroTextBox.Location = new System.Drawing.Point(89, 196);
+            this.bairroTextBox.Name = "bairroTextBox";
+            this.bairroTextBox.Size = new System.Drawing.Size(216, 20);
+            this.bairroTextBox.TabIndex = 16;
+            // 
+            // LocalidadeTextBox
+            // 
+            this.LocalidadeTextBox.Location = new System.Drawing.Point(89, 231);
+            this.LocalidadeTextBox.Name = "LocalidadeTextBox";
+            this.LocalidadeTextBox.Size = new System.Drawing.Size(216, 20);
+            this.LocalidadeTextBox.TabIndex = 17;
+            // 
+            // UfTextBox
+            // 
+            this.UfTextBox.Location = new System.Drawing.Point(89, 268);
+            this.UfTextBox.Name = "UfTextBox";
+            this.UfTextBox.Size = new System.Drawing.Size(39, 20);
+            this.UfTextBox.TabIndex = 18;
+            // 
+            // ibgeTextBox
+            // 
+            this.ibgeTextBox.Location = new System.Drawing.Point(89, 307);
+            this.ibgeTextBox.Name = "ibgeTextBox";
+            this.ibgeTextBox.Size = new System.Drawing.Size(100, 20);
+            this.ibgeTextBox.TabIndex = 19;
+            // 
+            // giaTextBox
+            // 
+            this.giaTextBox.Location = new System.Drawing.Point(89, 345);
+            this.giaTextBox.Name = "giaTextBox";
+            this.giaTextBox.Size = new System.Drawing.Size(100, 20);
+            this.giaTextBox.TabIndex = 20;
+            // 
+            // dddTextBox
+            // 
+            this.dddTextBox.Location = new System.Drawing.Point(89, 377);
+            this.dddTextBox.Name = "dddTextBox";
+            this.dddTextBox.Size = new System.Drawing.Size(35, 20);
+            this.dddTextBox.TabIndex = 21;
+            // 
+            // siafiTextBox
+            // 
+            this.siafiTextBox.Location = new System.Drawing.Point(89, 415);
+            this.siafiTextBox.Name = "siafiTextBox";
+            this.siafiTextBox.Size = new System.Drawing.Size(100, 20);
+            this.siafiTextBox.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(330, 450);
+            this.Controls.Add(this.siafiTextBox);
+            this.Controls.Add(this.dddTextBox);
+            this.Controls.Add(this.giaTextBox);
+            this.Controls.Add(this.ibgeTextBox);
+            this.Controls.Add(this.UfTextBox);
+            this.Controls.Add(this.LocalidadeTextBox);
+            this.Controls.Add(this.bairroTextBox);
+            this.Controls.Add(this.complementoTextBox);
+            this.Controls.Add(this.logradouroTextBox);
+            this.Controls.Add(this.cepTextBox);
+            this.Controls.Add(this.siafiLabel);
+            this.Controls.Add(this.dddLabel);
+            this.Controls.Add(this.giaLabel);
+            this.Controls.Add(this.ibgeLabel);
+            this.Controls.Add(this.ufLabel);
+            this.Controls.Add(this.localidadeLabel);
+            this.Controls.Add(this.bairroLabel);
+            this.Controls.Add(this.complementoLabel);
+            this.Controls.Add(this.logradouroLabel);
+            this.Controls.Add(this.cepLabelRetorno);
+            this.Controls.Add(this.ConsultaBtn);
+            this.Controls.Add(this.consultaCepTextBox);
+            this.Controls.Add(this.cepLabel);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label cepLabel;
+        private System.Windows.Forms.TextBox consultaCepTextBox;
+        private System.Windows.Forms.Button ConsultaBtn;
+        private System.Windows.Forms.Label cepLabelRetorno;
+        private System.Windows.Forms.Label logradouroLabel;
+        private System.Windows.Forms.Label complementoLabel;
+        private System.Windows.Forms.Label bairroLabel;
+        private System.Windows.Forms.Label localidadeLabel;
+        private System.Windows.Forms.Label ufLabel;
+        private System.Windows.Forms.Label ibgeLabel;
+        private System.Windows.Forms.Label giaLabel;
+        private System.Windows.Forms.Label dddLabel;
+        private System.Windows.Forms.Label siafiLabel;
+        private System.Windows.Forms.TextBox cepTextBox;
+        private System.Windows.Forms.TextBox logradouroTextBox;
+        private System.Windows.Forms.TextBox complementoTextBox;
+        private System.Windows.Forms.TextBox bairroTextBox;
+        private System.Windows.Forms.TextBox LocalidadeTextBox;
+        private System.Windows.Forms.TextBox UfTextBox;
+        private System.Windows.Forms.TextBox ibgeTextBox;
+        private System.Windows.Forms.TextBox giaTextBox;
+        private System.Windows.Forms.TextBox dddTextBox;
+        private System.Windows.Forms.TextBox siafiTextBox;
+    }
+}
+
---
+++ ConsultaCepTeste/ConsultaCepTeste/Form1.cs
@@ -0,0 +1,64 @@
+﻿using RestSharp;
+using System;
+using System.Web.Script.Serialization;
+using System.Windows.Forms;
+
+namespace ConsultaCepTeste
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void ConsultaBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var client = new RestClient(String.Format("https://viacep.com.br/ws/{0}/json/", consultaCepTextBox.Text));
+
+                var request = new RestRequest();
+
+                var response = await client.GetAsync(request);
+
+                string json = response.Content;
+
+                if (response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                {
+                    MessageBox.Show("Houve um problema ao consultar o CEP...");
+                }
+
+                else
+                {
+                    DadosRetornoAPI dadosRetornoAPI = new JavaScriptSerializer().Deserialize<DadosRetornoAPI>(json);
+
+                    cepTextBox.Text = dadosRetornoAPI.Cep;
+                    logradouroTextBox.Text = dadosRetornoAPI.Logradouro;
+                    complementoTextBox.Text = dadosRetornoAPI.Complemento;
+                    bairroTextBox.Text = dadosRetornoAPI.Bairro;
+                    LocalidadeTextBox.Text = dadosRetornoAPI.Localidade;
+                    UfTextBox.Text = dadosRetornoAPI.Uf;
+                    ibgeTextBox.Text = dadosRetornoAPI.Ibge;
+                    giaTextBox.Text = dadosRetornoAPI.Gia;
+                    dddTextBox.Text = dadosRetornoAPI.Ddd;
+                    siafiTextBox.Text = dadosRetornoAPI.Siafi;
+
+                    if (cepTextBox.Text == "")
+                    {
+                        MessageBox.Show("Houve um problema ao consultar o CEP...");
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("CEP inválido!", ex.Message);
+            }
+        }
+    }
+}
